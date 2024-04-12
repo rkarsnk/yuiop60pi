866a99a4f1d9d40ab0b359a7fca476419c9598c1
rkarsnk <00_g1thub01@rkarsnk.net>
2024-04-12 18:14:45 +0900

    change keymap for mac.

--- yuiop60pi/keymap.c
+++ yuiop60pi/keymap.c
@@ -10,7 +10,7 @@ const keycode_t keymaps[][KEY_NUM] = {
       KC_TAB      ,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,      KC_BSPC,
       KC_LCTL       ,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,             KC_ENT ,
       KC_LSFT           ,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT , KC_SLSH,         KC_RSFT, MO(1)  ,
-                   MO(1)   ,  KC_LALT ,                         KC_SPC                         ,   KC_RALT  , KC_RGUI
+                   KC_LALT   ,  KC_LGUI ,                         KC_SPC                         ,   KC_RGUI  , KC_RALT
     },
 
     [1] = {
